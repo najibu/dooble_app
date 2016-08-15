71e5b1270a6a2815a8bb07ff46ba862418a00198
najibu <ns.najibu@gmail.com>
2016-08-15 16:48:22 +0100

    Implemented adding comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true
 	validates :body, presence: true
 	belongs_to :user
+	has_many :comments, dependent: :destroy
 	
 	default_scope { order(created_at: :desc) }
 end
