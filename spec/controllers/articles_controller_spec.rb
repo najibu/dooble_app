2e09078f9f196b53325b0c0b10966f9c117405a2
najibu <ns.najibu@gmail.com>
2016-08-15 15:17:20 +0100

    Restrict access with controller actions

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,12 +1,40 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe ArticlesController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #edit" do
+    before do
+      @john = User.create(email: "john@example.com", password: "password")
+    end
+
+    context "owner is allowed to edit his article" do
+      it"renders the edit template" do
+        login_user @john
+        article = Article.create(title: "first article", body: "body", user: @john)
+
+        get :edit, id: article
+        expect(response).to render_template :edit
+      end
+    end
+
+    context "non-owner is not allowed to edit his article" do
+      it "redirects to the root paht" do
+        fred = User.create(email: "fred@example.com", password: "password")
+        login_user fred
+        article = Article.create(title: "first article", body: "body", user: @john)
+
+        get :edit, id: article
+        expect(response).to redirect_to(root_path)
+        message = "You can only edit your own article."
+        expect(flash[:danger]).to eq message
+
+      end
+    end
+
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
-
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,10 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 include Warden::Test::Helpers
-Warden.test_mode! 
+Warden.test_mode!
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
